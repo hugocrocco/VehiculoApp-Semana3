ae54a652d9ee9bf438397656d8643cd21377bbd5
hugocrocco <hugoacrocco@gmail.com>
2025-08-28 16:46:48 -0400

    Trabajo Semana 3 Hugo Valencia y Pablo Cavieres

---
+++ VehiculoApp.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class VehiculoApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Entrada de datos
+        System.out.print("Marca: ");
+        String marca = sc.nextLine();
+
+        System.out.print("Modelo: ");
+        String modelo = sc.nextLine();
+
+        System.out.print("Cilindrada (cc): ");
+        int cilindrada = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Tipo de Combustible: ");
+        String combustible = sc.nextLine();
+
+        System.out.print("Capacidad en pasajeros: ");
+        int capacidad = Integer.parseInt(sc.nextLine());
+
+        // Salida de datos
+        System.out.println("La marca que ha ingresado es: " + marca);
+        System.out.println("El modelo que ha ingresado es: " + modelo);
+        System.out.println("La cilindrada que ha ingresado es: " + cilindrada);
+        System.out.println("El tipo de combustible es: " + combustible);
+        System.out.println("Tiene una capacidad de " + capacidad + " pasajeros.");
+
+        sc.close();
+    }
+}
